4507983eaee44eb0560716a06335873ab1cdb8f5
hirooze <hirooze@gmail.com>
2021-08-25 22:22:33 +0900

    dev commit

--- batch/test.bat
+++ batch/test.bat
@@ -1,7 +1,18 @@
 @echo off
-rem test
-echo test
+rem タイムスタンプ関連
+echo ----------------
+echo タイムスタンプ
+echo ----------------
+
+rem ----------------
 rem ログ用の時間定義
-set dt=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%
+rem ----------------
+
+rem timeの空白文字を0に変換
+set time2=%time: =0%
+
+rem dt にタイムスタンプを定義する
+set dt=%date:~0,4%%date:~5,2%%date:~8,2%%time2:~0,2%%time2:~3,2%
+
 echo %dt%
 pause
