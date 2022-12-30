8bcdc1c698040e6693ae8fd7fe07c135cbe49882
hirooze <hirooze@gmail.com>
2022-12-30 11:11:09 +0900

    add powershell pg

---
+++ powershell/GetFolderInfo.ps1
@@ -0,0 +1,26 @@
+﻿<# Windows Power Shell GetFolderInfo #>
+
+## 初期値設定
+$dt=date -f yyyyMMddHHmmss
+
+## 保存場所
+# カレントディレクトリに出力
+$output = Convert-Path .
+# デスクトップに出力
+#$output = [System.Enviroment]::Get-FolderPath("Desktop")
+# 指定のフォルダに出力
+#$output = 
+
+## 情報取得対象フォルダ (<$output>を任意のパスに変更する。)
+$target = $output
+$filename = (Get-Item $target).Name
+
+## ログ取得開始
+Start-Transcript -Path $output\log_$dt.log
+
+# 処理内容
+Get-Childitem -Recurse $target | Select-Object FullName,CreationTime,LastWriteTime,LastaccessTime,Length | Export-Csv -NoTypeInformation -Encoding Default $output\$filename-$dt.csv
+
+## ログ取得終了
+stop-transcript
+pause
---
+++ powershell/template.ps1
@@ -0,0 +1,24 @@
+﻿<# Windows Power Shell テンプレート #>
+
+## 初期値設定
+$dt=date -f yyyyMMddHHmmss
+
+## 保存場所
+# カレントディレクトリに出力
+$output = Convert-Path .
+# デスクトップに出力
+#$output = [System.Enviroment]::Get-FolderPath("Desktop")
+# 指定のフォルダに出力
+#$output = 
+
+## ログ取得開始
+Start-TranScript -Path $output\log_$dt.log
+
+# 処理内容
+
+echo $output
+echo $dt
+
+## ログ取得終了
+Stop-Transcript
+pause
