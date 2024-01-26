b1f16df2a11aeecc3a4bd4ad0aa8107f5882942a
@hirooze3594 <hirooze@gmail.com>
2024-01-26 14:59:56 +0900

    commit at 24,1/26

--- powershell/MultiStringReplace/MultiStringReplace.ps1
+++ powershell/MultiStringReplace/MultiStringReplace.ps1
@@ -1,37 +1,38 @@
-﻿<# Windows Power Shell テンプレート #>
+﻿<# Windows PowerShell Program #>
 ## 保存場所
 # カレントディレクトリに出力
 $path           = Split-Path -Parent $MyInvocation.MyCommand.Path
-$impconf        = $path + "\" + "setup.conf"
-$imptxt         = $path + "\" + "imp.txt"
-$exptxt         = $path + "\" + "output.txt"
+$impFolder      = $path + "\imp"
+$impconf        = $path + "\setup.conf"
+$outFolder      = $path
 $ImpRepPatterns = Import-Csv -Path $impconf -Encoding utf8
-$ImpReptext     = Get-Content -Path $imptxt -Encoding utf8
+# impフォルダ内のテキストファイルを取得
+$impFiles       = Get-ChildItem -Path $impFolder -Filter *.txt
 
-$outputData = @()
-## ログ取得開始
-#Start-TranScript -Path $output\log_$dt.log
-# 処理内容
-# 変換後のデータを格納するための配列
-# 各行の文字列を置換
-foreach ($row in $ImpReptext) {
-    $originalValue = $row
+foreach ($impFile in $impFiles) {
+    $ImpReptext = Get-Content -Path $impFile.FullName -Encoding utf8
+    # 変換後のデータを格納するための配列
+    $outputData = @()
 
-    for ($i = 0; $i -lt $ImpRepPatterns.Count; $i++) {
-        $pattern = $ImpRepPatterns[$i].before
-        $replace = $ImpRepPatterns[$i].after
+    # 各行の文字列を置換
+    foreach ($row in $ImpReptext) {
+        $originalValue = $row
 
-        if ($originalValue -like "*$pattern*") {
-            # 文字列に指定したパターンが含まれている場合に置換
-            $originalValue = $originalValue -replace [regex]::Escape($pattern), $replace
+        foreach ($pattern in $ImpRepPatterns) {
+            if ($originalValue -like "*$($pattern.before)*") {
+                # 文字列に指定したパターンが含まれている場合に置換
+                $originalValue = $originalValue -replace [regex]::Escape($pattern.before), $pattern.after
+            }
         }
+        # 変換後の行を配列に追加
+        $outputData += $originalValue
     }
-    # 変換後の行を配列に追加
-    $outputData += $originalValue
+    # 出力先のファイルパスを組み立て
+    $outputFilePath = Join-Path -Path $outFolder -ChildPath $impFile.Name
+
+    # 変換後のデータを新しいテキストファイルに書き込む
+    $outputData | Out-File -FilePath $outputFilePath -Encoding utf8
+
+    # 処理が完了したらメッセージを表示
+    Write-Host "File Replaced. $($impFile.Name)                          `t出力先: $($outputFilePath)"
 }
-# 変換後のデータを新しいテキストファイルに書き込む
-$outputData | Out-File -FilePath $exptxt
-# 変換後のファイルを表示
-Get-Content $exptxt
-## ログ取得終了
-#Stop-Transcript
